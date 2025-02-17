38674fc224c97adfc5954b3f831e0c208da2988a
LiudmylaIermolenko <lussi.yer@gmail.com>
2025-02-17 08:31:45 +0100

    d/z TasksList_Set_Collection_Person

---
+++ src/main/java/org/asd/D_Z_Tasks/Person.java
@@ -0,0 +1,43 @@
+package org.asd.D_Z_Tasks;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + " лет)";
+    }
+
+        public static void main(String[] args) {
+            // Создаем LinkedList с объектами Person
+            LinkedList<Person> people = new LinkedList<>();
+            people.add(new Person("Анна", 25));
+            people.add(new Person("Борис", 35));
+            people.add(new Person("Виктор", 40));
+            people.add(new Person("Дарья", 28));
+            people.add(new Person("Егор", 22));
+
+            System.out.println("Исходный список:");
+            System.out.println(people);
+
+            Iterator<Person> iterator = people.iterator();
+            while (iterator.hasNext()) {
+                if (iterator.next().age > 30) {
+                    iterator.remove();
+                }
+            }
+
+            System.out.println("\nСписок после удаления:");
+            System.out.println(people);
+        }
+    }
+
