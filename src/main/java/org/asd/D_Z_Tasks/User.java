c0a2b58b3c6eea64f9a3f175b7e3d142b2ca88a8
LiudmylaIermolenko <lussi.yer@gmail.com>
2025-03-04 08:11:15 +0100

    d/z User

---
+++ src/main/java/org/asd/D_Z_Tasks/User.java
@@ -0,0 +1,131 @@
+package org.asd.D_Z_Tasks;
+
+public class User {
+
+       private String firstName;
+       private String lastName;
+       private String email;
+       private String phone;
+       private int age;
+
+    public User(String firstName, String lastname, String email, String phone, int age) {
+        this.firstName = firstName;
+        this.lastName = lastname;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " - " + email + "; " + phone + "; " + " age: " + age;
+    }
+}
+
+
+
+
+
+
+
+
+//Реализовать класс User с полями firstName, lastName, email, phone, age.
+//
+//          Написать класс UserParser, с статическим методом parseUsers который обрабатывает строку следующего формата,
+//        и возвращает список юзеров.
+//
+//
+//В случае не корректных данных (Прим: Mike_007), этот юзер в список попадать не должен.
+//
+//
+//String document = “””
+//        [User]
+//
+//Name: John Smith
+//
+//Email: john.smith@example.com
+//
+//Phone: +44-7911-123456
+//
+//Age: 28
+//
+//
+//
+//        [User]
+//
+//Name: Emily Johnson
+//
+//Email: emily.j@example.co.uk
+//
+//Phone: +49-1521-9876543
+//
+//Age: 35
+//
+//
+//
+//        [User]
+//
+//Name: William Brown
+//
+//Email: william.brown@mail.net
+//
+//Phone: +33-6-12-34-56-78
+//
+//Age: 42
+//
+//
+//
+//        [User]
+//
+//Name: Mike_007
+//
+//Email: mike007@website
+//
+//Phone: +34-654-321
+//
+//Age: 16
+//
+//
+//
+//        [User]
+//
+//Name: Jessica Davis
+//
+//Email: jessica.davis@example.org
+//
+//Phone: +31-612345678
+//
+//Age: 24
+//
+//
+//
+//        [User]
+//
+//Name: Thomas Anderson
+//
+//Email: thomas@matrix.com
+//
+//Phone: +41-78-987-65-43
+//
+//Age: 35
+//        “””;
---
+++ src/main/java/org/asd/D_Z_Tasks/UserParser.java
@@ -0,0 +1,69 @@
+package org.asd.D_Z_Tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserParser {
+    public static List<User> parseUsers(String text) {
+        List<User> users = new ArrayList<>();
+        String[] usersData = text.replaceAll("\\s+\\[User]", "\n").trim().split("\n{2,}");
+
+        Pattern namePattern = Pattern.compile("[A-Z][a-z]+\\s[A-Z][a-z]+");
+        Pattern emailPattern = Pattern.compile("[\\w.]+@[\\w.]+\\.(ru|com|net|org|uk)");
+        Pattern phonePattern = Pattern.compile("\\+[\\d-]{12,}");
+        Pattern agePattern = Pattern.compile("\\b([1-9][0-9]?)\\b");
+
+        for (String userData : usersData) {
+            String firstName = null;
+            String lastName = null;
+            String email = null;
+            String phone = null;
+            int age = -1;
+
+            //.group() возвращает найденное совпадение ("John Smith"),.split(" ") разбивает строку по пробелу, создавая массив:
+            //nameParts = ["John", "Smith"];
+            Matcher nameMatcher = namePattern.matcher(userData);
+            if (nameMatcher.find()) {
+                String[] nameParts = nameMatcher.group().split(" ");
+                firstName = nameParts[0];
+                lastName = nameParts[1];
+            }
+                Matcher emailMatcher = emailPattern.matcher(userData);
+                if (emailMatcher.find()) {
+                    email = emailMatcher.group();
+                }
+
+                Matcher phoneMatcher = phonePattern.matcher(userData);
+                if (phoneMatcher.find()) {
+                    phone = phoneMatcher.group();
+                }
+
+                Matcher ageMatcher = agePattern.matcher(userData);
+                if (ageMatcher.find()) {
+                    age = Integer.parseInt(ageMatcher.group());
+                }
+
+                if (firstName != null && lastName != null && email != null && phone != null && age > 0) {
+                    users.add(new User(firstName, lastName, email, phone, age));
+                }
+            }
+        return users;
+        }
+
+
+    }
+
+
+
+
+//Реализовать класс User с полями firstName, lastName, email, phone, age.
+//
+//          Написать класс UserParser, с статическим методом parseUsers который
+//        обрабатывает строку следующего формата,
+//        и возвращает список юзеров.
+//
+//
+//В случае не корректных данных (Прим: Mike_007), этот юзер в список попадать не должен.
+//
